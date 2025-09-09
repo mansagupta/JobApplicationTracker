e8ddf9e9efd8cbe48f3767194fc1510842e1e64f
mansagupta <nymansa.gupta@gmail.com>
2025-09-09 08:52:23 -0700

    Initial commit

---
+++ src/main/java/com/example/jobApplicationTracker/JobApplicationTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobApplicationTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicationTrackerApplication.class, args);
+	}
+
+}
